e11a7d56fa89f7ad2d53ddc7b8df5133d0a0b608
Roman Zaryuta <zel_smer4@bk.ru>
2024-01-28 11:44:37 +0300

    Creat list and add (while prototype) function

--- main.cpp
+++ main.cpp
@@ -8,18 +8,26 @@ using namespace std;
 struct salary
 {
     string name = "Coworker";
-    string birth_date = "dd.mm.yyyy";
+    string issue_date = "dd.mm.yyyy";
     double salary = 0;
 };
 
 void list_salary(ifstream& file_out, salary& company)
 {
-    int len;
-    file_out.read((char*)&len, sizeof(len));
-    company.name.resize(len);
-    file_out.read((char*)company.name.c_str(), len);
-    file_out.read((char*)company.birth_date.c_str(), len);
+    int len_name;
+    int len_date;
+    file_out.read((char*)&len_name, sizeof(len_name));
+    company.name.resize(len_name);
+    file_out.read((char*)company.name.c_str(), len_name);
+    file_out.read((char*)&len_date, sizeof(len_date));
+    company.issue_date.resize(len_date);
+    file_out.read((char*)company.issue_date.c_str(), len_date);
     file_out.read((char*)&company.salary, sizeof(company.salary));
+}
+
+void add_salary(ofstream& file_in, salary& company)
+{
+
 }
 
 int main()
@@ -33,14 +41,21 @@ int main()
     if (command == "list")
     {
         ifstream file_out ("salary.txt");
-        for (int i = 0; !file_out.eof(); i++)
+        list_salary(file_out, company);
+        while(file_out.eof())
         {
-            list_salary(file_out, company);
+            cout << company.name << " " << company.issue_date << " " << company.salary << endl;
         }
+        file_out.close();
     }
-
-    for (int i = 0; i < sizeof(company); i++);
+    else if (command == "add")
     {
-        cout << company.name  << company.birth_date << company.salary << endl;
+        ofstream file_in ("salary.txt", ios::app);
+
+        file_in.close();
+    }
+    else
+    {
+        cerr << "Unknown command!" << endl;
     }
 }
